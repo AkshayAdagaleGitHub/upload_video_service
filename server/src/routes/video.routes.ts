a1a986414848e52418c8cb236ac41bd9fd6adb35
Akshay A <akshayadagale@gmail.com>
2025-05-31 00:31:37 -0700

    updating server and client

--- server/src/routes/video.routes.ts
+++ server/src/routes/video.routes.ts
@@ -1,9 +1,9 @@
-import * as express from 'express';
-import { authMiddleware } from '../middleware/authMiddleware';
-// import { uploadVideo } from '../controllers/video.controllers';
-
-const router = express.Router();
-
-// router.post('/upload', authMiddleware, uploadVideo); // Protect the /upload route
-
-export default router;
+// import * as express from 'express';
+// import { authMiddleware } from '../middleware/authMiddleware';
+// // import { uploadVideo } from '../controllers/video.controllers';
+//
+// const router = express.Router();
+//
+// // router.post('/upload', authMiddleware, uploadVideo); // Protect the /upload route
+//
+// export default router;
